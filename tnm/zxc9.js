87991e2d9241c76256e89d9c30e814de8efeddac
spikewuller <spikewuller@gmail.com>
2018-12-11 00:02:12 +0700

    no message

--- tnm/zxc9.js
+++ tnm/zxc9.js
@@ -1,3 +1,4 @@
+var currentAngle=Math.floor(Math.random() * 44) + 1;
 var theWheel = new Winwheel({
     'rotationAngle': Math.floor(Math.random() * 359),
     'numSegments'  : 8,     // Specify number of segments.
@@ -16,7 +17,7 @@ var theWheel = new Winwheel({
         ],
     'animation' :           // Specify the animation to use.
         {
-            'stopAngle': Math.floor(Math.random() * 44) + 1,
+            'stopAngle': currentAngle,
             'type'     : 'spinToStop',
             'duration' : 5,     // Duration in seconds.
             'spins'    : 8,     // Number of complete spins.
@@ -40,6 +41,36 @@ function startSpin()
         theWheel.startAnimation();
         wheelSpinning = true;
     }
+}
+
+function reset() {
+    var oldAngle=currentAngle;
+    currentAngle=Math.floor(Math.random() * 44) + 1;
+    theWheel = new Winwheel({
+        'rotationAngle': oldAngle,
+        'numSegments'  : 8,     // Specify number of segments.
+        'outerRadius'  : 212,   // Set outer radius so wheel fits inside the background.
+        'textFontSize' : 28,    // Set font size as desired.
+        'segments'     :        // Define segments including colour and text.
+            [
+                {'fillStyle' : '#ff834e', 'text' : '10.000 VND'},
+                {'fillStyle' : '#aaff3e', 'text' : '300.000 VND'},
+                {'fillStyle' : '#fcffb6', 'text' : '30.000 VND'},
+                {'fillStyle' : '#ff90a9', 'text' : '500.000 VND'},
+                {'fillStyle' : '#97a5ff', 'text' : '100.000 VND'},
+                {'fillStyle' : '#53fff6', 'text' : '50.000 VND'},
+                {'fillStyle' : '#b559ff', 'text' : '20.000 VND'},
+                {'fillStyle' : '#b1ff11', 'text' : '200.000 VND'}
+            ],
+        'animation' :           // Specify the animation to use.
+            {
+                'stopAngle': currentAngle,
+                'type'     : 'spinToStop',
+                'duration' : 5,     // Duration in seconds.
+                'spins'    : 8,     // Number of complete spins.
+                'callbackFinished' : alertPrize
+            }
+    });
 }
 
 var app = angular.module('SinglePage', []);
