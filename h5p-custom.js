65a7080395c62c864040342038c8ba8240459b75
spikewuller <spikewuller@gmail.com>
2021-08-23 14:58:07 +0700

    no message

---
+++ h5p-custom.js
@@ -0,0 +1,39 @@
+console.log('Hello Custom File is running!!!!!!!')
+var currentEmail;
+var tempCurrentEmail = localStorage.getItem(
+    `current_account`
+);
+if (tempCurrentEmail) {
+    currentEmail = JSON.parse(tempCurrentEmail);
+} else {
+    console.log("current_account was not existed!")
+}
+var tempLs;
+var ls = window.localStorage.getItem(`user_info_${currentEmail}`);
+if (ls) {
+    tempLs = JSON.parse(ls);
+} else {
+    console.log("user_info was not existed!")
+}
+var token = tempLs.access_token;
+console.log(token);
+if (isSetupAlready === undefined) {
+    isSetupAlready = true;
+    var H5P = window.H5P = window.H5P || {};
+    H5P.jQuery.ajaxSetup({
+        headers: { 'Authorization': `Bearer ${token}` }
+    });
+
+    const originalFetch = window.fetch;
+    window.fetch = function () {
+        arguments[1].headers = { 'Authorization': `Bearer ${token}` };
+        return originalFetch.apply(this, arguments)
+    };
+
+    const open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function (method, url, ...rest) {
+        var recall = open.call(this, method, url, ...rest);
+        this.setRequestHeader('Authorization', `Bearer ${token}`);
+        return recall;
+    };
+}
